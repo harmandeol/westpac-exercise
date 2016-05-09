175706c346484d971c11e77046b14201d3c5d8fb
harman deol <harman.deol@cba.com.au>
2016-05-09 16:54:59 +1000

    fix: IE issue where bar was not displayed

--- js/directives/progressBar.Directive.js
+++ js/directives/progressBar.Directive.js
@@ -17,7 +17,7 @@ app.directive("progressBar", ["$timeout", function ($timeout) {
         },
         template: ' <div> \
                         <div class="progress"> \
-                            <div class="progress-bar ng-class:{\'progress-bar-completed\': isCompleted };"  aria-valuenow="{{complete}}" aria-valuemin="0" aria-valuemax="{{total}}" style="width:{{progress * 100}}%;"></div> \
+                            <div class="progress-bar ng-class:{\'progress-bar-completed\': isCompleted };"  aria-valuenow="{{complete}}" aria-valuemin="0" aria-valuemax="{{total}}" ng-style="{ width: progress*100 + \'%\' }"></div> \
                         </div> \
                         <div class="progress-summary" ng-show="!isCompleted" >{{completeLabel}} {{showPercent ? progress*100 : complete | number:0}}{{showPercent?"%":""}}</div> \
                         <div class="progress-summary" ng-show="isCompleted" >The Task is 100% completed <i class="fa fa-check" aria-hidden="true"></i></div> \
